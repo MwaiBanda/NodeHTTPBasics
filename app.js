2725013336742eab6f9158545b2f164cbf94fc0e
Mwai Banda <bandamwai@gmail.com>
2023-04-30 15:33:10 -0500

    Added ejs

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+// register view engine
+app.set('view engine', 'ejs')
+
+app.listen(3000)
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+//   res.render('./views/index.ejs', { root: __dirname})
+    const blogs = [
+      {title: "The Begining Of The End", snippet: "A fictional story on the begining of the end of the world"},
+      {title: "To Start You Have To Stop", snippet: "A suspense filled dramatic comedy on everyday life"}
+
+    ]
+    res.render('index', {blogs})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create')
+})
+// redirects
+app.get("/about-us", (req, res) => {
+  res.redirect('/about')
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404')
+})
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const http = require('http')
 const fs = require('fs')
+const lodash = require('lodash')
 
 const server = http.createServer((req, res) => {
-    console.log(`request made`)
-    console.log(req.url, req.method)
+
+    // random numbers from loadash 
+    const num = _.random(0, 20)
+    console.log(`Random ${num}`)
+
     res.setHeader('Content-Type', 'text/html')
     let path = "./views/"
+
+    // Run once
+    const greet = _.once(() => {
+      console.log('Greetings!')
+    })
+
+    greet()
+    greet()
 
     switch(req.url) {
         case "/": path += "index.html"; res.statusCode = 200; break;
         case "/about": path += "about.html"; res.statusCode = 200; break;
-        case "/about-me":
+        case "/about-us":
             res.statusCode = 301
             res.setHeader('Location', '/about')
             break
