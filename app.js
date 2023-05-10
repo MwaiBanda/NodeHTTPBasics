e2adf8e5c837a24e673ea7f6764230805d360423
Mwai Banda <bandamwai@gmail.com>
2023-05-10 00:13:38 -0500

    Added blog routes

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog =  require('./models/blogs')
 const app = express()
-
-import morgan from 'morgan'
-import mongoose from 'mongoose'
 
 // register view engine
 app.set('view engine', 'ejs')
-
-app.listen(3000)
+app.use(morgan('dev'))
 app.use(express.static('public'));
+
+const DB_URL = "mongodb+srv://mwaideveloper:playground@playground.zde6mdx.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(DB_URL)
+.then(() => {
+  app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+  })
+})
+.catch((err) => {
+  console.log(err)
+})
+
+
 
 app.get('/', (req, res) => {
 //   res.render('./views/index.ejs', { root: __dirname})
@@ -32,7 +44,56 @@ app.get("/about-us", (req, res) => {
   res.redirect('/about')
 })
 
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404')
 })
+
+
